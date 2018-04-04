9badd1c574bb4c407cf1d3cc294719aa680d4a60
zerl <starss@outlook.com>
2018-04-04 20:54:33 +0800

    add 1001 1002

---
+++ pat_advanced/A+B for Polynomials.cpp
@@ -0,0 +1,29 @@
+#include "pat_advanced.h"
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<int,double> polys;
+//map<int,double>::reverse_iterator iter;
+
+int i1002(){
+    int in_count,exp;
+    double coe;
+
+    cin >> in_count;
+    while (in_count--) {
+        cin >> exp >> coe;
+        polys[exp]=coe;
+    }
+    cin >> in_count;
+    while (in_count--) {
+        cin >> exp >> coe;
+        polys.count(exp)==1?polys[exp]+=coe:polys[exp]=coe;
+        polys[exp]==0?polys.erase(exp):0;
+    }
+    cout << polys.size();
+    for( auto iter = polys.rbegin() ; iter !=polys.rend();iter++)
+        printf(" %d %.1lf",iter->first,iter->second);
+    cout<<endl;
+    return 0;
+}
---
+++ pat_advanced/A+B_Format.cpp
@@ -0,0 +1,16 @@
+#include "pat_advanced.h"
+#include<iostream>
+using namespace std;
+
+int i1001() {
+    int a,b;
+    cin >> a >> b;
+    string s = to_string(a + b);
+    long long len = s.length();
+    for (int i = 0; i < len; i++) {
+        cout << s[i];
+        if (s[i] == '-') continue;
+        if ((i + 1) % 3 == len % 3 && i != len - 1) cout << ",";
+    }
+    return 0;
+}
